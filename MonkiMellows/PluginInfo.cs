4f6c0154b79bf63011fd3e91e02d883560ec1354
Graze <reece2004f@gmail.com>
2022-05-30 05:37:23 +0100

    Add project files.

---
+++ MonkiMellows/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkiMellows
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.monkimellows";
+        public const string Name = "MonkiMellows";
+        public const string Version = "1.0.0";
+    }
+}
